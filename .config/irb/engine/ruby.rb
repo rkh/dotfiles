753a11a5564eac94b00d672e2c836c2a88ecb14e
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2009-01-20 01:19:42 +0800

    fixed var name
    Signed-off-by: Konstantin Haase <konstantin.mailinglists@googlemail.com>

--- .config/irb/engine/ruby.rb
+++ .config/irb/engine/ruby.rb
@@ -3,8 +3,8 @@ module MyIRB
   def get_source an_object
     case an_object
     when String then an_object
-    when Module then Ruby2Ruby.translate object
-    when Method then Ruby2Ruby.translate object.owner, object.name
+    when Module then Ruby2Ruby.translate an_object
+    when Method then Ruby2Ruby.translate an_object.owner, object.name
     else raise ArgumentError, "Don't know how to get source of #{an_object}"
     end
   end
