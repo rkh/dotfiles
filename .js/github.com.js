d278bd99176cad447ce603179912ef691f8daa5d
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2011-09-22 12:01:40 -0700

    minimize github toolbar

--- .js/github.com.js
+++ .js/github.com.js
@@ -1,1 +1,8 @@
-$().ready(function() { $('.octofication').remove(); });
+$('.octofication').remove();
+$("body").append("<style> .userbox .unread_count  { display: none !important; } </style>");
+$('.usernav li:first').remove();
+$('.usernav li:first').remove();
+$('.avatarname img').remove();
+$('.topsearch').css({'margin-right': 80, 'clear': 'none'});
+$('#header').css({height: 60});
+$('.logo img').css({height: 35, "margin-top": -12});
